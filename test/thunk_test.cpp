ebb96bc5b57aed7bc2f3099218859c87e8d3028b
Tobias Loew <tobi@die-loews.de>
2024-04-06 14:34:00 +0200

    Update thunk_test.cpp

--- test/thunk_test.cpp
+++ test/thunk_test.cpp
@@ -10,16 +10,26 @@ public:
     {
         return a + b;
     }
+
+    void foo() {
+        auto add = m_thunk.func();
+        auto result = add(1, 2);
+        BOOST_TEST_EQ(3, result);
+    }
+
+    lunaticpp::thunk<LUNATIC_THUNKS_MAKE_CALL(SimpleObject::add)> m_thunk{ this };
 };
 
 void member_func_test()
 {
     SimpleObject dt;
     lunaticpp::thunk<LUNATIC_THUNKS_MAKE_CALL(SimpleObject::add)> thunk(&dt);
-    int (*add)(int,int)=thunk.func();
+    auto add = thunk.func();
 
     auto result=add(1,2);
     BOOST_TEST_EQ(3,result);
+
+    dt.foo();
 }
 
 void std_function_test()
@@ -27,7 +37,7 @@ void std_function_test()
     using func_type = int(int, int);
     std::function<func_type> f=[](int a, int b){return a+b;};
     lunaticpp::thunk< LUNATIC_THUNKS_MAKE_CALL(std::function<func_type>::operator ())> thunk(&f);
-    int (*add)(int,int)=thunk.func();
+    auto add = thunk.func();
 
     auto result=add(1,2);
     BOOST_TEST_EQ(result, 3);
